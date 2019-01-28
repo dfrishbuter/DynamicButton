6afa7dee2796b8ceef753f1232afdbc753f7c49e
Dmitry Frishbuter <dfrishbuter@ronasit.com>
2019-01-28 12:30:49 +0600

    Generate project

---
+++ DynamicButton/DynamicButton.h
@@ -0,0 +1,16 @@
+//
+//  Created by Dmitry Frishbuter on 28/01/2019
+//  Copyright © 2019 Dmitry Frishbuter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicButton.
+FOUNDATION_EXPORT double DynamicButtonVersionNumber;
+
+//! Project version string for DynamicButton.
+FOUNDATION_EXPORT const unsigned char DynamicButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicButton/PublicHeader.h>
+
+
---
+++ Sources/Foo.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Dmitry Frishbuter on 28/01/2019
+//  Copyright © 2019 Dmitry Frishbuter. All rights reserved.
+//
+
+import Foundation
+
+open class Foo {
+
+    public init() {
+
+    }
+}
